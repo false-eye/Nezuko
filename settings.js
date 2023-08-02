0a33044925acbef0a4df9cc0b05b5a7136cab709
fundachi <105562781+false-eye@users.noreply.github.com>
2023-08-02 15:03:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ if (!gg) {
 }
 
 global.owner = gg.split("919539901834");
-global.mongodb = process.env.MONGODB || "mongodb+srv://falseye:<password>@cluster0.lzobdyr.mongodb.net/?retryWrites=true&w=majority";
+global.mongodb = process.env.MONGODB || "mongodb+srv://falseye:aslamhlm@cluster0.lzobdyr.mongodb.net/?retryWrites=true&w=majority";
 global.prefa = process.env.PREFIX || ".";
 global.ggle = process.env.GOOGLE_API || "none";
 global.wter = process.env.WEATHER_API || "none";
