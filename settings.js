7991ae7287c6522105fc3bcf89c4f8f3edad5d5d
fundachi <105562781+false-eye@users.noreply.github.com>
2023-08-02 15:02:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,11 +3,11 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983, 923087880256";
+  gg = "919539901834";
 }
 
-global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "none";
+global.owner = gg.split("919539901834");
+global.mongodb = process.env.MONGODB || "mongodb+srv://falseye:<password>@cluster0.lzobdyr.mongodb.net/?retryWrites=true&w=majority";
 global.prefa = process.env.PREFIX || ".";
 global.ggle = process.env.GOOGLE_API || "none";
 global.wter = process.env.WEATHER_API || "none";
